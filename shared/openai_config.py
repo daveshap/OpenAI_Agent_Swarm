71b5d5ff1d5e0b43d116f4984d9b9c587d1055e0
alex <alexwstraw@gmail.com>
2023-11-11 23:03:57 +0000

    PR Feedback: Use Pydantic to create a central place to load env vars from

--- shared/openai_config.py
+++ shared/openai_config.py
@@ -1,11 +1,6 @@
-import os
-import dotenv
+from shared.settings import Settings
 from openai import OpenAI
 
-dotenv.load_dotenv()
-
 def get_openai_client():
-    api_key = os.getenv('OPENAI_API_KEY')
-    if api_key is None:
-        raise ValueError('The OPENAI_API_KEY environment variable is not set.')
-    return OpenAI(api_key=api_key)
+    settings = Settings()
+    return OpenAI(api_key=settings.OPENAI_API_KEY)
